a7cbadfbd471c60fa8f867bda1b0f594bec4a312
michellelee1 <mlee3@colum.edu>
2020-12-12 00:42:13 -0600

    js edit

--- wear/main.js
+++ wear/main.js
@@ -1,4 +1,18 @@
+let slideMask = false;
+
 $('#mask').click(function(){
+      
+  if('mask') {
     $(this).addClass("slide-mask");
-  })
+  }else {
+    $(this).addClass("slide-mask-down");
+  }
+    
+  slideMask = !slideMask;
+    })
+
+
+//$('#mask').click(function(){
+//    $(this).addClass("slide-mask");
+//  })
 
