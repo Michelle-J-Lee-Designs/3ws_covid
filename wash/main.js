1f8e3e53aca1de9703046da493722cc8f694fda2
michellelee1 <mlee3@colum.edu>
2020-12-16 20:52:13 -0600

    push

--- wash/main.js
+++ wash/main.js
@@ -1,11 +1,10 @@
 $('#spout').click(function(){
-  
   $('#spout').addClass("turn-on-water");
   })
 
   $('#spout').on("animationend", function(){
     $('#spout').removeClass("turn-on-water");
-  })
-
+    })
+  
 
 
