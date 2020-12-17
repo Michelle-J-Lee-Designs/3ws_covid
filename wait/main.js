191e0fa331a23a71dde76f2a75f0bd96ab0e9ed3
michellelee1 <mlee3@colum.edu>
2020-12-16 18:51:56 -0600

    push

--- wait/main.js
+++ wait/main.js
@@ -1,3 +1,16 @@
 $('#figure-two').click(function(){
-    $('#figure-two').addClass("distance");
-  })
+      $('#figure-two').addClass("moveAway");
+      })
+
+//  $('#figure-two').click(function(){
+//    let move = true;
+//    if(move) {
+//    removeClass(moveIn)
+//    addClass(moveAway);
+//      move = false;
+//     } else {
+//        removeClass(moveAway);
+//        addClass(moveIn);
+//        move = true;
+//    }
+//   })
--- wash/main.js
+++ wash/main.js
@@ -1,3 +1,11 @@
 $('#spout').click(function(){
-    $('#spout').addClass("turn-on-water");
+  
+  $('#spout').addClass("turn-on-water");
   })
+
+  $('#spout').on("animationend", function(){
+    $('#spout').removeClass("turn-on-water");
+  })
+
+
+
