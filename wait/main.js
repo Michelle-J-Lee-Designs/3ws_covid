c152e59f75135aa89b441494a6b7cf10a148ff15
michellelee1 <mlee3@colum.edu>
2021-08-30 12:49:22 -0500

    change

--- wait/main.js
+++ wait/main.js
@@ -1,16 +1,3 @@
 $('#figure-two').click(function(){
       $('#figure-two').addClass("moveAway");
-      })
-
-//  $('#figure-two').click(function(){
-//    let move = true;
-//    if(move) {
-//    removeClass(moveIn)
-//    addClass(moveAway);
-//      move = false;
-//     } else {
-//        removeClass(moveAway);
-//        addClass(moveIn);
-//        move = true;
-//    }
-//   })
+     })
